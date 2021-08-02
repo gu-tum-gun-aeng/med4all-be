bee5acd3b943fa7a9c0d7a2fbf9d704cee17acb8
veerapat.b <veerapat.b@ava.fund>
2021-08-02 18:12:29 +0700

    removed unit test

--- tests/unit/controllers/patient.controller.test.ts
+++ tests/unit/controllers/patient.controller.test.ts
@@ -7,9 +7,3 @@ Deno.test("PatientController should response with mock data", async () => {
   await PatientController.patients(mockContext);
   assertEquals(mockContext.response.body, { results: [] });
 });
-
-Deno.test("PatientController should upload image files", async () => {
-  const mockContext = testing.createMockContext();
-  await PatientController.uploadImages(mockContext);
-  assertEquals(mockContext.response.body, { results: [] });
-});
