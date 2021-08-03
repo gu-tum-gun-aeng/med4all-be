e31a20bb6de68a89dc69bf28b6ce31e3f8025335
Saravuth Ruangsutham <saravuth.r@ava.fund>
2021-08-03 07:38:36 +0700

    fix import

--- src/utils/db.util.ts
+++ src/utils/db.util.ts
@@ -1,8 +1,5 @@
-import {
-  QueryArrayResult,
-  QueryObjectResult,
-} from "https://deno.land/x/postgres@v0.11.3/query/query.ts";
-import { Pool, PoolClient } from "../../deps.ts";
+
+import { Pool, PoolClient, QueryArrayResult, QueryObjectResult } from "../../deps.ts";
 import configs from "../config/config.ts";
 
 const pool = new Pool(
