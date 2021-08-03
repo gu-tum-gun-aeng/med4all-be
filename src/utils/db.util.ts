e1f52e743699ff572848b0b35caf19b301d26a7d
Saravuth Ruangsutham <saravuth.r@ava.fund>
2021-08-03 07:36:32 +0700

    format code

--- src/utils/db.util.ts
+++ src/utils/db.util.ts
@@ -1,24 +1,29 @@
-import { QueryArrayResult, QueryObjectResult } from "https://deno.land/x/postgres@v0.11.3/query/query.ts";
+import {
+  QueryArrayResult,
+  QueryObjectResult,
+} from "https://deno.land/x/postgres@v0.11.3/query/query.ts";
 import { Pool, PoolClient } from "../../deps.ts";
 import configs from "../config/config.ts";
 
-const pool = new Pool(configs.dbConnectionString, configs.dbConnectionPool, true);
+const pool = new Pool(
+  configs.dbConnectionString,
+  configs.dbConnectionPool,
+  true,
+);
 
 const DbUtil = {
-  
   queryObject: async <T>(statement: string) => {
-      const client: PoolClient = await pool.connect();
-      let result: QueryObjectResult<T>;
-      try {
-        result = await client.queryObject<T>(
-          statement,
-        );
-        return result.rows;
-      } finally {
-        await client.release();
-      }
-     
-    },
+    const client: PoolClient = await pool.connect();
+    let result: QueryObjectResult<T>;
+    try {
+      result = await client.queryObject<T>(
+        statement,
+      );
+      return result.rows;
+    } finally {
+      await client.release();
+    }
+  },
   queryArray: async <T extends Array<unknown>>(statement: string) => {
     const client: PoolClient = await pool.connect();
     let result: QueryArrayResult<T>;
@@ -30,8 +35,7 @@ const DbUtil = {
     } finally {
       await client.release();
     }
-    
-  }
-}
+  },
+};
 
 export default DbUtil;
