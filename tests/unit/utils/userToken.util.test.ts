a649cb3ae9f549f1d828ef6743894865316bf0ba
Natechawin <natechawin@gmail.com>
2021-08-03 17:24:31 +0700

    refactor: refact jwt util to userToken util
    Win, Orm, Yort

--- tests/unit/utils/userToken.util.test.ts
+++ tests/unit/utils/userToken.util.test.ts
@@ -1,12 +1,14 @@
-import { assertEquals, stub } from "../../../deps.ts";
-import * as jwt from "../../../src/utils/jwt.util.ts";
+import { assertEquals } from "../../../deps.ts";
+import { createUserToken } from "../../../src/utils/userToken.util.ts";
 
 Deno.test("createToken should create Token with key when given key to the function", async () => {
   const key = "someKey";
+  const userPayload = { id: 123 };
+
   const expectedToken =
     "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzfQ.IjNmcIjZ7w3kIITfFW9oqawU87mHpXjspttbteFG25285ATEWuw4uKNDgrKWtvIkEPrPOf7UtBORE3foYKKgzg";
 
-  const actualResult = await jwt.createUserToken({ id: 123 }, key);
+  const actualResult = await createUserToken(userPayload, key);
 
   assertEquals(expectedToken, actualResult);
 });
