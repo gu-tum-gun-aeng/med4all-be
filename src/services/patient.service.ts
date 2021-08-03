fde0d4b3065ac415dac62f48662ae14cc7bc2826
catfly-tawan-w <c.catfly@gmail.com>
2021-08-03 10:54:34 +0700

    feature: PatientService.getAll is now use patient data from DB (Warning: Please fix test)
    Tawan, Orm, Praem, Bell

--- src/services/patient.service.ts
+++ src/services/patient.service.ts
@@ -2,18 +2,14 @@ import { traceWrapperAsync } from "../utils/trace.util.ts";
 import patientRepository from "../repositories/patient.repository.ts";
 import { Patient } from "../models/patient/patient.model.ts";
 
-//Todo: remove this list and add the data to db instead
-const patients: Patient[] = [];
-
 export const getPatients = async (): Promise<Patient[]> => {
-  // const patient = await patientRepository.getAll();
-  // console.log(patient);
   return await traceWrapperAsync<Patient[]>(
-    async () => await patients,
+    async () => await patientRepository.getAll(),
     "route",
   );
 };
 
+// TODO: Implement this to connect to db instead.
 export const addPatients = async (patient: Patient) => {
-  await patients.push(patient);
+  return await 0;
 };
