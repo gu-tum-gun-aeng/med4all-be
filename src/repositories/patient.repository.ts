aeb031e311169b0c7c70fffbdec9df92712715e8
Saravuth Ruangsutham <saravuth.r@ava.fund>
2021-08-04 10:05:15 +0700

    modify dbUtil to support multiple statement transaction

--- src/repositories/patient.repository.ts
+++ src/repositories/patient.repository.ts
@@ -1,12 +1,12 @@
 import { Patient } from "../models/patient/patient.model.ts";
 import dbUtils from "../utils/db.util.ts";
 
-const SELECT_ALL_PATIENTS =
-  "SELECT patient_id, name, age, weight_kg, height_cm, certificate_id, certificate_type_id, certificate_picture_url, covid_test_picture_url, medical_info FROM patient";
-
 const PatientRepository = {
   getAll: async () => {
-    return await dbUtils.queryObject<Patient>(SELECT_ALL_PATIENTS);
+    const query = dbUtils.toQuery
+      `SELECT patient_id, name, age, weight_kg, height_cm, certificate_id, certificate_type_id, certificate_picture_url,
+    covid_test_picture_url, medical_info FROM patient`;
+    return await dbUtils.queryObject<Patient>(query);
   },
 };
 
