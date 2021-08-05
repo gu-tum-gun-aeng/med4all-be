e9c989855a2dc9e9f26ac279f17039ab08bf17e3
Natechawin <natechawin@gmail.com>
2021-08-05 09:34:11 +0700

    feat: add issuer claim check

--- tests/unit/utils/token.util.test.ts
+++ tests/unit/utils/token.util.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "../../../deps.ts";
-import { verify } from "https://deno.land/x/djwt@v2.2/mod.ts";
+import { create, Payload, verify } from "https://deno.land/x/djwt@v2.2/mod.ts";
 import * as tokenUtil from "../../../src/utils/token.util.ts";
 
 Deno.test("createToken should create valid Token with given token id when call", async () => {
@@ -16,7 +16,7 @@ Deno.test("createToken should create valid Token with given token id when call",
   assertEquals(payload.jti, tokenInfo.id.toString());
 });
 
-Deno.test("isValid should return true when given token is not expired yet", async () => {
+Deno.test("isValid should return true when given token is not expired and issuer cliam is correct", async () => {
   const key = "someKey";
   const tokenInfo = {
     id: 123,
@@ -43,6 +43,32 @@ Deno.test("isValid should return false when given token is already expired", asy
   };
 
   const token = await tokenUtil.createToken(tokenInfo, key);
+  const isValid = await tokenUtil.isValid(
+    token,
+    key,
+    tokenUtil.HashAlgorithm.HS512,
+  );
+
+  assertEquals(isValid, false);
+});
+
+Deno.test("isValid should return false when given token issuer claim is incorrect", async () => {
+  const key = "someKey";
+
+  const issueDateTime = tokenUtil.getNumericDateFrom(new Date().getTime());
+  const payload: Payload = {
+    jti: "1",
+    iss: "some other issuer",
+    ist: issueDateTime,
+    exp: issueDateTime + 60,
+  };
+
+  const token = await create(
+    { alg: tokenUtil.HashAlgorithm.HS512, typ: "JWT" },
+    payload,
+    key,
+  );
+
   const isValid = await tokenUtil.isValid(
     token,
     key,
