8fc3d6f6b3121dc6de7479c5edaab9fbd47f73cb
Natechawin <natechawin@gmail.com>
2021-08-06 01:40:23 +0700

    test: add test for some config which need string manipulation

--- tests/unit/config/config.test.ts
+++ tests/unit/config/config.test.ts
@@ -15,3 +15,31 @@ Deno.test("config should be able to get related config from env file when OS env
   assertEquals(config.nexmo.apiKey, "someNexmoApiKey");
   assertEquals(config.nexmo.ApiSecret, "someNexmoApiSecret");
 });
+
+Deno.test("config should be able to get related config from env file when OS env variable didn't set", () => {
+  assertEquals(config.appName, "med4all-be-test");
+
+  assertEquals(config.jwt.key, "someKey");
+  assertEquals(config.jwt.ttlSeconds, 60);
+
+  assertEquals(config.s3.accessKeyID, "someS3AccessKey");
+  assertEquals(config.s3.secretKey, "someS3SecretKey");
+  assertEquals(config.s3.bucketName, "someBucketName");
+  assertEquals(config.s3.region, "someS3Region");
+
+  assertEquals(config.nexmo.apiKey, "someNexmoApiKey");
+  assertEquals(config.nexmo.ApiSecret, "someNexmoApiSecret");
+});
+
+Deno.test("config.dbConnectionString should return correct connection string when all required config are present", () => {
+  const expected =
+    `postgresql://someDbUserName:someDbPassword@someDbHost:5432/med4all?sslmode=prefer`;
+
+  assertEquals(config.dbConnectionString, expected);
+});
+
+Deno.test("config.url should return correct url when all required config are present", () => {
+  const expected = `http://localhost:8000`;
+
+  assertEquals(config.url, expected);
+});
