dd6f0d9779c6f41995bc3acf9b09eb8e25431fa5
Nakarin Kakanumporn <belizwp@live.com>
2021-08-16 17:06:48 +0700

    feat: add api get swagger yaml schema

---
+++ src/routers/v1/swagger.router.ts
@@ -0,0 +1,8 @@
+import { Router } from "../../../deps.ts";
+import SwaggerController from "../../controllers/swagger.controller.ts"
+
+const router = new Router();
+
+router.get("/swagger.yaml", SwaggerController.getYamlSchema)
+
+export default router;
