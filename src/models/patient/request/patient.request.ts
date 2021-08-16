c4eece1a320a4da89b2275b34a856194a315cdc8
yort <k.kanchanapiboon@gmail.com>
2021-08-16 16:17:05 +0700

    changed CreatePatientRequest to new version

--- src/models/patient/request/patient.request.ts
+++ src/models/patient/request/patient.request.ts
@@ -7,21 +7,91 @@ import {
   startsWith,
   ValidationRules,
 } from "../../../../deps.ts";
+
 export type CreatePatientRequest = {
-  patientName: string;
-  age: number;
+  certificateId: string;
+  certificateType: ENUM;
+  name: string;
+  surname: string;
+  gender: ENUM;
+  ageYear: number;
+  patientPhone: string;
+  custodianPhone: string;
   weightKg: number;
   heightCm: number;
-  certificateId: string;
-  certificateTypeId: number;
-  certificatePictureUrl: string;
-  covidTestPictureUrl: string;
-  address: string;
-  district: string;
-  province: string;
-  zipCode: number;
-  medicalInfo?: Record<string, unknown>;
-};
+  isAtkPositive: boolean;
+  isRtPcrPositive: boolean;
+  labTestWhen: Date;
+  isFavipiravirReceived: boolean;
+  receivedFavipiravirWhen: Date;
+  bodyTemperatureCelcius: number;
+  pulseRateBpm: number;
+  oxygenSaturation: number;
+  oxygenSaturationAfterExercise: number;
+  oxygenSaturationDifference: number;
+  systolic: number;
+  diastolic: number;
+  inspirationRate: number;
+  isPregnant: boolean;
+  pregnancyWeeks: number;
+  isBedridden: boolean;
+  symptoms: string;
+  allergyToDrugs: string[];
+  allergyToFoods: string[];
+  isSymptomShortnessOfBreath: boolean;
+  isSymptomFever: boolean;
+  isSymptomCough: boolean;
+  isSymptomRunnyNose: boolean;
+  isSymptomSoreThroat: boolean;
+  isSymptomFatigue: boolean;
+  isSymptomHeadAche: boolean;
+  isSymptomDiarrhea: boolean;
+  isSymptomLossOfSmell: boolean;
+  isSymptomConjunctivitis: boolean;
+  isSymptomRash: boolean;
+  isSymptomLossOfTaste: boolean;
+  isSymptomTiredness: boolean;
+  isSymptomChestPain: boolean;
+  isDiseaseUncontrollDm: boolean;
+  isDiseaseCancer: boolean;
+  isDiseaseCopd: boolean;
+  isDiseaseAsthma: boolean;
+  isDiseaseObesity: boolean;
+  isDiseaseCkdLevelHigherThanFour: boolean;
+  isDiseaseStrokeWithinSixMonth: boolean;
+  isDiseaseCardioVascularDisease: boolean;
+  isDiseaseHiv: boolean;
+  isDiseaseHypertension: boolean;
+  isDiseaseHyperlipidemia: boolean;
+  isDiseaseCirrhosis: boolean;
+  isDiseaseTuberculosis: boolean;
+  checkInDate: Date;
+  checkOutDate: Date;
+  addressProvince: string;
+  addressDistrict: string;
+  addressSubDistrict: string;
+  addressMoo: string;
+  addressRoad: string;
+  addressAlley: string;
+  addressSoi: string;
+  addressVillage: string;
+  addressBangkokZone: string;
+  addressBuilding: string;
+  addressNote: string;
+  patientDataSource: ENUM;
+  admittedTo: ENUM;
+  healthCoverage: ENUM;
+  lineId: string;
+  homeTown: ENUM;
+  equipments: ENUM[];
+  vaccinationRecords: ENUM[];
+  firstVaccinedDate: Date;
+  secondVaccinedDate: Date;
+  remark: string;
+  firstDateOfSymtom: Date;
+  createdDate: Date;
+}
+
 
 export const CreatePatientRequestValidationSchema: ValidationRules = {
   patientName: [required, minLength(1)],
