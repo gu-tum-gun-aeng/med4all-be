76d073cbb9cb760358be2c70cad8ceb04e66a0d2
Nakarin Kakanumporn <belizwp@live.com>
2021-08-18 21:30:15 +0700

    feat: add api get swagger-ui

--- src/controllers/swagger.controller.ts
+++ src/controllers/swagger.controller.ts
@@ -1,6 +1,10 @@
 import { Context } from "../../deps.ts";
 
 const SwaggerController = {
+  getSwaggerUi: ({ response }: Context) => {
+    const swaggerFileUrl = encodeURIComponent("/v1/swagger.yaml");
+    response.redirect(`https://generator.swagger.io/?url=${swaggerFileUrl}`);
+  },
   getYamlSchema: async ({ response }: Context) => {
     const swaggerFile = await Deno.readTextFile("./api-swagger.yaml");
     response.headers.set("Content-Type", "application/x-yaml");
--- src/routers/v1/swagger.router.ts
+++ src/routers/v1/swagger.router.ts
@@ -3,6 +3,7 @@ import SwaggerController from "../../controllers/swagger.controller.ts";
 
 const router = new Router();
 
+router.get("/api-docs", SwaggerController.getSwaggerUi);
 router.get("/swagger.yaml", SwaggerController.getYamlSchema);
 
 export default router;
