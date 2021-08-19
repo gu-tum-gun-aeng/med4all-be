2399f76be5e49f2320d9a6547cfb7b8b6c5f3ee1
Nakarin Kakanumporn <belizwp@live.com>
2021-08-19 09:53:52 +0700

    chore:move swagger api to root path

--- src/routers/v1/swagger.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "../../../deps.ts";
-import SwaggerController from "../../controllers/swagger.controller.ts";
-
-const router = new Router();
-
-router.get("/api-docs", SwaggerController.getSwaggerUi);
-router.get("/swagger.yaml", SwaggerController.getYamlSchema);
-
-export default router;
--- tests/integration/swagger.test.ts
+++ tests/integration/swagger.test.ts
@@ -1,6 +1,10 @@
 import { superdeno } from "../../deps.ts";
 import app from "../../src/app.ts";
 
-Deno.test("when call /v1/swagger.yaml, it should return 200 status", async () => {
-  await superdeno(app.handle.bind(app)).get("/v1/swagger.yaml").expect(200);
+Deno.test("when call /swagger.yaml, it should return 200 status", async () => {
+  await superdeno(app.handle.bind(app)).get("/swagger.yaml").expect(200);
+});
+
+Deno.test("when call /api-docs, it should redirected", async () => {
+  await superdeno(app.handle.bind(app)).get("/api-docs").expect(302);
 });
