87e88813ac99c388970ecaaada876631df6f5a8b
Natechawin <natechawin@gmail.com>
2021-09-14 14:02:55 +0700

    Revert "hotfix: comment out colink call"
    This reverts commit dd07deb932c895c77ebeebf753d66ad8281774f8.

--- tests/integration/patient.test.ts
+++ tests/integration/patient.test.ts
@@ -1,16 +1,16 @@
 import { superdeno } from "../../deps.ts";
 import app from "../../src/app.ts";
-// import PatientRepository from "../../src/dataaccess/database/patient.repository.ts";
-// import PatientApiService from "../../src/dataaccess/service/patient-api/patient-api.service.ts";
-// import { stub } from "../../deps.ts";
-// import { patientRequestMock } from "../mock/patient/patient.request.mock.ts";
-// import * as tokenUtil from "../../src/utils/token/token.util.ts";
-// import config from "../../src/config/config.ts";
-// import { mockPublishPatientResponse } from "../mock/patient-api/publishPatient.response.mock.ts";
-// import ColinkApiService from "../../src/dataaccess/service/colink-api/colink-api.service.ts";
-// import { mockColinkApiCheckStatusDuplicatePatientResponse } from "../mock/colink/colink.response.mock.ts";
-// import VolunteerRepository from "../../src/dataaccess/database/volunteer.repository.ts";
-// import { mockVolunteer } from "../mock/volunteer/volunteer.mock.ts";
+import PatientRepository from "../../src/dataaccess/database/patient.repository.ts";
+import PatientApiService from "../../src/dataaccess/service/patient-api/patient-api.service.ts";
+import { stub } from "../../deps.ts";
+import { patientRequestMock } from "../mock/patient/patient.request.mock.ts";
+import * as tokenUtil from "../../src/utils/token/token.util.ts";
+import config from "../../src/config/config.ts";
+import { mockPublishPatientResponse } from "../mock/patient-api/publishPatient.response.mock.ts";
+import ColinkApiService from "../../src/dataaccess/service/colink-api/colink-api.service.ts";
+import { mockColinkApiCheckStatusDuplicatePatientResponse } from "../mock/colink/colink.response.mock.ts";
+import VolunteerRepository from "../../src/dataaccess/database/volunteer.repository.ts";
+import { mockVolunteer } from "../mock/volunteer/volunteer.mock.ts";
 
 Deno.test("when call /v1/patients with invalid token, it should return 401", async () => {
   const mockToken = "FAKE_TOKEN";
@@ -26,64 +26,64 @@ Deno.test("when call /v1/patients without Authorization header, it should return
     .expect(401);
 });
 
-// Deno.test("when call post /v1/patient, it should return error when patient is already exist in colink system", async () => {
-//   const expectedResult =
-//     await mockColinkApiCheckStatusDuplicatePatientResponse();
+Deno.test("when call post /v1/patient, it should return error when patient is already exist in colink system", async () => {
+  const expectedResult =
+    await mockColinkApiCheckStatusDuplicatePatientResponse();
 
-//   const stubPatientRepositoryGetPatientRegisterStatus = stub(
-//     PatientRepository,
-//     "getPatientRegisterStatus",
-//     [await { is_registered: false }],
-//   );
+  const stubPatientRepositoryGetPatientRegisterStatus = stub(
+    PatientRepository,
+    "getPatientRegisterStatus",
+    [await { is_registered: false }],
+  );
 
-//   const stubPatientRepository = stub(
-//     PatientRepository,
-//     "createPatient",
-//     [10],
-//   );
+  const stubPatientRepository = stub(
+    PatientRepository,
+    "createPatient",
+    [10],
+  );
 
-//   const stubPatientApiService = stub(
-//     PatientApiService,
-//     "publishPatient",
-//     [await mockPublishPatientResponse],
-//   );
+  const stubPatientApiService = stub(
+    PatientApiService,
+    "publishPatient",
+    [await mockPublishPatientResponse],
+  );
 
-//   const stubColinkApiService = stub(
-//     ColinkApiService,
-//     "checkStatus",
-//     [expectedResult],
-//   );
+  const stubColinkApiService = stub(
+    ColinkApiService,
+    "checkStatus",
+    [expectedResult],
+  );
 
-//   const stubVolunteerRepository = stub(
-//     VolunteerRepository,
-//     "getVolunteerById",
-//     await mockVolunteer,
-//   );
+  const stubVolunteerRepository = stub(
+    VolunteerRepository,
+    "getVolunteerById",
+    await mockVolunteer,
+  );
 
-//   const mockToken = await tokenUtil.createToken({
-//     id: "1",
-//     hashAlgorithm: tokenUtil.HashAlgorithm.HS512,
-//     ttlSeconds: 60,
-//   }, config.jwt.key);
-//   try {
-//     await superdeno(app.handle.bind(app))
-//       .post("/v1/patients")
-//       .set("Authorization", `Bearer ${mockToken}`)
-//       .send(patientRequestMock)
-//       .expect(200)
-//       .expect({
-//         results: {
-//           error: {
-//             id: 1002,
-//             message: "Patient is already exist in Colink system.",
-//           },
-//         },
-//       });
-//   } finally {
-//     stubPatientRepositoryGetPatientRegisterStatus.restore();
-//     stubPatientRepository.restore();
-//     stubPatientApiService.restore();
-//     stubColinkApiService.restore();
-//     stubVolunteerRepository.restore();
-//   }
-// });
+  const mockToken = await tokenUtil.createToken({
+    id: "1",
+    hashAlgorithm: tokenUtil.HashAlgorithm.HS512,
+    ttlSeconds: 60,
+  }, config.jwt.key);
+  try {
+    await superdeno(app.handle.bind(app))
+      .post("/v1/patients")
+      .set("Authorization", `Bearer ${mockToken}`)
+      .send(patientRequestMock)
+      .expect(200)
+      .expect({
+        results: {
+          error: {
+            id: 1002,
+            message: "Patient is already exist in Colink system.",
+          },
+        },
+      });
+  } finally {
+    stubPatientRepositoryGetPatientRegisterStatus.restore();
+    stubPatientRepository.restore();
+    stubPatientApiService.restore();
+    stubColinkApiService.restore();
+    stubVolunteerRepository.restore();
+  }
+});
